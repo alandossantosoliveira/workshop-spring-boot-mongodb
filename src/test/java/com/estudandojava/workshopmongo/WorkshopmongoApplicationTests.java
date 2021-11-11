c7cf9e4750af883f201d6850ed7c02e248389c45
alan oliveira <alan89oliveira@gmail.com>
2021-11-11 09:23:03 -0300

    Primeiro commit - projeto criado

---
+++ src/test/java/com/estudandojava/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudandojava.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
