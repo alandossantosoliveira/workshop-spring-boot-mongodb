2adc1bebc08fc2ba57827c12d985dcfe12bf69d4
alan oliveira <alan89oliveira@gmail.com>
2021-11-13 10:59:18 -0300

    Obtendo um usuário por id e tratamento de exceção personalizado

--- src/main/java/com/estudandojava/workshopmongo/services/UserService.java
+++ src/main/java/com/estudandojava/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.estudandojava.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.estudandojava.workshopmongo.domain.User;
 import com.estudandojava.workshopmongo.repository.UserRepository;
+import com.estudandojava.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -18,4 +20,16 @@ public class UserService {
 		return repo.findAll();
 	}
 
+	/*
+	public User findById(String id) {
+		User user = repo.findOne(id);
+		if (user == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado");
+		}
+		return user;
+	}*/
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
 }
