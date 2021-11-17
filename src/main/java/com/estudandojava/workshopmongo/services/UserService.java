8b650b6388c7519108e0a1f4278a735afc157b82
alan oliveira <alan89oliveira@gmail.com>
2021-11-17 09:39:58 -0300

    Deleção de usuario com DELETE

--- src/main/java/com/estudandojava/workshopmongo/services/UserService.java
+++ src/main/java/com/estudandojava/workshopmongo/services/UserService.java
@@ -41,4 +41,9 @@ public class UserService {
 	public User fromDTO(UserDTO obj) {
 		return new User(obj.getId(), obj.getName(), obj.getEmail());
 	}
+	
+	public void delete(String id) {
+		findById(id);
+		repo.deleteById(id);
+	}
 }
